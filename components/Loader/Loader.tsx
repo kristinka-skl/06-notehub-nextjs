4bd230ba10f90b23897efca2651e4e03ffbf49a4
kristinka-skl <kristinka.skl@gmail.com>
2025-10-20 13:41:48 +0200

    ready structure

---
+++ components/Loader/Loader.tsx
@@ -0,0 +1,8 @@
+import css from "./Loader.module.css";
+export default function Loader() {
+  return (
+    <div className={css.loaderWrapper}>
+      <div className={css.loader}></div>
+    </div>
+  );
+}
